d4a47fdc9cd016bc844898acf059cb87ea4d152b
Gulnozai Khodizoda <gul.khodizoda@gmail.com>
2020-01-21 20:53:59 -0500

    added check to verify if av elements and length is valid

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 int     main(int ac, char *av[])
 {
     if (ac == N + 1)
-        solve_sudoku(av);
+    {
+        if (is_valid(av))
+            solve_sudoku(av);
+        else
+            write(1, "Error\n", 6);
+    }
     else
         write(1, "Error\n", 6);
     return (0);
