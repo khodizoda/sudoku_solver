ec13c098ba223434d29f24b1c5f269c871414b61
Gulnozai Khodizoda <gul.khodizoda@gmail.com>
2020-01-21 21:05:52 -0500

    upd f-n to be under 25 lines

--- solve_sudoku.c
+++ solve_sudoku.c
@@ -6,21 +6,18 @@ void    solve_sudoku(char *av[])
     int i;
     int c;
 
-    r = 0;
-    i = 1;
-    while (r < N)
+    r = -1;
+    i = 0;
+    while (++r < N && ++i <= N)
     {
-        c = 0;
-        while (c < N)
+        c = -1;
+        while (++c < N)
         {
             if (av[i][c] == '.')
                 BOARD[r][c] = 0;
             else
                 BOARD[r][c] = av[i][c] - '0';
-            c++;
         }
-        r++;
-        i++;
     }
     if (solver(BOARD))
         print_sudoku(BOARD);
